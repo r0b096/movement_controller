824cbbf333c675a91f4734ab19e180d72935ed02
r0b096 <148765254+r0b096@users.noreply.github.com>
2023-10-23 16:09:38 +0530

    Add files via upload

---
+++ MovementController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float speed = 6f;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
